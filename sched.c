a342306106ef3a101682fce646b7f9a4cdad7cd6
kei1_surface <>
2021-06-13 16:40:41 +0900

    commnet out some lines

--- sched.c
+++ sched.c
@@ -1,11 +1,11 @@
 #include <sys/types.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <time.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <err.h>
+//#include <err.h>
 //commit from ubuntu from surface
 
 #define NLOOP_FOR_ESTIMATION 1000000000UL
